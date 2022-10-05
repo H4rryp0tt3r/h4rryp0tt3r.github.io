66ec46f3c0279700cf66e75564b726176c6f2d61
Nagesh Podilapu <nagesh.podilapu@gmail.com>
2022-10-05 23:36:37 +0200

    Set default font to Inter font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   darkMode: 'class',
   content: ["./templates/**/*.html"],
-  theme: {},
+  theme: {
+    fontFamily: {
+      'sans': ['Inter', 'Helvetica', 'sans-serif'],
+    }
+  },
   variants: {},
   plugins: [
       require('@tailwindcss/typography'),
